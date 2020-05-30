7d382a8356b13f39f5aa4bb9acabb6972fe0eb6a
Prabhnoor <psratanpal@hotmail.com>
2020-05-30 19:12:41 -0400

    Revert "First Commit"
    This reverts commit b99db391e486c57c3b250aa4ba40521ccd5900c5.

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: 'Ping!',
-    execute(message, args) {
-        message.channel.send('Pong.');
-        //console.log(message);
-        console.log(args);
-    },
-};
