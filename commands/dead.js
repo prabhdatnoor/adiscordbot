a3da981486c6058305afdf0eab280f22e4c49b19
Prabhnoor <prabhdatnoor@users.noreply.github.com>
2020-08-05 13:29:22 -0400

    v 1.0.1
    cleaned up some code in coin and dead command. Deleted toEmbed.js - really no need for it

--- commands/dead.js
+++ commands/dead.js
@@ -1,45 +1,33 @@
 const shuffle = require('../my_functions/shuffle.js');
 const Discord = require('discord.js');
-const toEmbed = require('../my_functions/toEmbed.js')
 const getMembers = require('../my_functions/getMembers.js');
-//console.log('yuh');
+
 module.exports = { //group,method,number
     name: 'dead',
     description: '',
+    aliases: ['deadbd', 'day'],
+
     execute(message, args) {
         var members = getMembers(message);
-        if (members > 2) {
-            emb_members = toEmbed(deadbd(members));
+
+        if (members.length >= 2) {
+            shuffle(members);
 
             const embed = new Discord.MessageEmbed()
                 .setColor('#ff0000')
                 .setTitle('Dead By Daylight Game')
-                .setDescription('Killer is ' + emb_members.splice(0, 1)[0]['value']);
+                .setDescription('Killer is ' + members.splice(0, 1))
+                .addField({ name: 'Survivors', value: members.splice(0, 4), inline: true });
 
-            if (emb_members.length > 0) {
-                embed.addFields(emb_members);
+            if (members.length > 0) {
+                embed.addField({
+                    name: 'Spectators',
+                    value: members.splice(0, members.length),
+                    inline: true
+                });
             }
-
-            message.channel.send(embed);
         } else {
-            message.channel.send("need more than 2 ppl fam");
+            message.channel.send('need atleast 2 ppl');
         }
-
     }
 };
-//var members = [1, 2, 3, 4, 5, 6];
-//console.log(deadbd(members));
-
-function deadbd(members) {
-    shuffle(members);
-    //console.log('yo' + members);
-    var game = [];
-
-    game['killer'] = members.splice(0, 1);
-    game['survivors'] = members.splice(0, 4);
-    if (members.length > 0 && members !== undefined) {
-        game['spectators'] = members;
-    }
-
-    return game;
-}
