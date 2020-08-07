4be0dcd8d1a37fdab8f90776ac22cc1123b085df
Prabhnoor <prabhdatnoor@users.noreply.github.com>
2020-08-06 22:29:49 -0400

    v 1.1.0
    fixed deadbd. Rewrote game command. Added bot filter on get members module. Will add more the game command soon!

--- commands/game.js
+++ commands/game.js
@@ -1,54 +1,84 @@
-const config = require('../my_configs/team_gen.json');
 const Discord = require('discord.js');
-const sortleftovers = config.main['sortleftovers'];
-const toEmbed = require('../my_functions/toEmbed.js')
-const embedconv = true;
+//var j = ['bruh', 'yes', 'okay', 'jheez', 'bama', 'dolores', 'julian', 'b', 'a', 'b', '1'];
+const shuffle = require('../my_functions/shuffle.js');
 const getMembers = require('../my_functions/getMembers.js');
+const game_config = require('../my_configs/team_gen.json');
+const toEmbed = game_config.main.convertembed;
+const lsort = game_config.main.sortleftovers;
+const dTeams = game_config.byteam.default_no_of_teams;
 
-const bypergroup = require('../my_modules/rtg_bypergroup.js');
-const byteams = require('../my_modules/rtg_byteams')
-const hexcode = require('../my_functions/randhex.js');
-
-const nice = [1, 2, 3, 4, 5, 6, 7];
-
-module.exports = { //group,method,number
+module.exports = {
     name: 'game',
-    description: '',
-    aliases: ['teams', 'team'],
+    description: 'randomizes teams. Default no. of teams is 2.',
+    aliases: ['teams', 'team', 'groups', 'group', 'rtg'],
     execute(message, args) {
-        //console.log('bruh1');
-        var method = args[0];
-        var number = args[1];
 
-        var members = getMembers(message);
+        var j = getMembers(message);
 
-        if (members.length > 2) {
+        console.log(j);
 
-            if (method === undefined && number === undefined) {
-                //console.log('MAIN');
-                console.log(members);
-                members = byteams(members, undefined, sortleftovers, embedconv);
-                //return nice;
-            } else if (method === 'byteams') {
-                members = byteams(members, number, sortleftovers, embedconv);
-            } else if (method === 'bypergroup') {
-                message.channel.send('work in progress');
-                //members = bypergroup(members, number, sortleftovers, embedconv);
-            } else {
-                message.channel.send('command not recognized');
-            }
-            //console.log('1' + members);
-            //console.log('2' + toEmbed(members));
-            const embed = new Discord.MessageEmbed()
-                .setColor(hexcode())
-                .setTitle('Groups')
-                .addFields(toEmbed(members));
+        /*if (args[0] === 'byteams') {//will add soon
+            dTeams = args[1];
+        }*/
+
+        if (j.length >= 2) {
+            var embed = new Discord.MessageEmbed()
+                .addFields(byteams(j, dTeams, toEmbed, lsort))
+                .setTitle('Groups are Made!')
+                .setColor('RANDOM');
 
             message.channel.send(embed);
-            //console.log('sent teams');
         } else {
-            message.channel.send('need more than 2ppl fam');
+            message.channel.send('Need at least 2 ppl fam');
         }
 
     }
 };
+
+function byteams(people, teams, isEmbed, sortleftover) {
+
+    var ppl_per_team = Math.floor(people.length / teams);
+
+    var leftover = people.length % teams;
+
+    shuffle(people);
+
+    var output = {};
+
+    for (var i = 0; i < teams; i++) {
+        output['Team ' + (i + 1)] = people.splice(0, ppl_per_team);
+    }
+
+    if (leftover > 0 && people.length > 0 && sortleftover === true) {
+        var i = 0;
+        do {
+            output['Team ' + (i + 1)].push(people.splice(0, 1)[0]);
+
+            i++;
+        }
+
+        while (i < teams && people.length > 0);
+    }
+
+    if (isEmbed === true) {
+        //console.log('yes');
+        return DisEmbed(output);
+    } else {
+        return output;
+    }
+
+}
+
+function DisEmbed(teams) {
+
+    var emb_out = [];
+
+    var no_of_teams = Object.keys(teams).length;
+
+    for (var i = 0; i < no_of_teams; i++) {
+
+        emb_out[i] = { name: Object.keys(teams)[i], value: Object.values(teams)[i], inline: true };
+    }
+
+    return emb_out;
+}
--- my_functions/getMembers.js
+++ my_functions/getMembers.js
@@ -1,13 +1,27 @@
 const Discord = require('discord.js');
+const filterbots = require('../my_configs/bot_config.json').settings.filterbots;
+
 module.exports = (message) => {
 
     const numPlayers = [];
 
-    message.member.voice.channel.members.forEach(key => {
-        //if (key.user.bot === false) {
-        numPlayers.push(key.user.username);
-        //}
-    });
+    if (filterbots === false) {
+        message.member.voice.channel.members.forEach(key => {
+
+            numPlayers.push(key.user.username);
+
+        });
+    } else {
+
+        message.member.voice.channel.members.forEach(key => {
+
+            if (key.user.bot === false) {
+                numPlayers.push(key.user.username);
+            }
+        });
+    }
+
+
     return numPlayers;
 
 };
