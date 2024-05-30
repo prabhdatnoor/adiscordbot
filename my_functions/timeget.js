28284511f75a1941fb022cfc8584ac92acc428fa
Prabhnoor <>
2024-05-30 19:16:08 -0400

    First commit
    files uploaded for the first time

---
+++ my_functions/timeget.js
@@ -0,0 +1,14 @@
+module.exports = (what) => {
+    var date = new Date();
+    if (what === 'stamp') {
+
+    } else if (what === '')
+        switch (what) {
+            case 'stamp':
+                date.get
+        }
+
+}
+
+var date = new Date();
+console.log(date.getTime());
