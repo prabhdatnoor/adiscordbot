9ef812f89324b65edcc4a30e8d93ab535bf01d8a
Prabhnoor <prabhdatnoor@users.noreply.github.com>
2024-08-05 13:29:22 -0400

    v 1.0.1
    cleaned up some code in coin and dead command. Deleted toEmbed.js - really no need for it

--- commands/coin.js
+++ commands/coin.js
@@ -1,21 +1,25 @@
 const Discord = require('discord.js');
+var coin = {
+    'heads': {
+        'name': 'Heads!',
+        'color': 'RED'
+    },
+    'tails': {
+        'name': 'Tails!',
+        'color': 'GREEN'
+    }
+};
 
 module.exports = {
     name: "coin",
     description: "just flips",
     aliases: "flip",
     execute(message, args) {
-        var sides = ['Heads!', 'Tails!'];
-
-        var coin = sides[Math.floor(Math.random() * 2)]
-
-        const embed = new Discord.MessageEmbed();
-
-        embed.setTitle(coin);
-        embed.setAuthor('Flipped!');
-        embed.setColor('RANDOM');
-
+        var side = coin[Object.keys(coin)[Math.floor(Math.random() * 2)]];
+        const embed = new Discord.MessageEmbed()
+            .setTitle(side.name)
+            .setAuthor('Flipped!')
+            .setColor(side.color);
         message.channel.send(embed);
-
     }
 }
