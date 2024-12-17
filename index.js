eb96d0aef9c12271ac57569f42e4b677f823ab5c
Prabhnoor <prabhdatnoor@users.noreply.github.com>
2024-12-17 13:22:29 -0500

    Added comments to index.js

--- index.js
+++ index.js
@@ -14,27 +14,31 @@ for (const file of commandFiles) {
     client.commands.set(command.name, command);
 }
 
+//prefix for command
 const prefix = require('./my_configs/bot_config.json').prefix;
-//const d_key = require('./my_configs/secret_stuff.json')["d-key"]; //use if hosting locally
-const d_key = process.env.diskey; //for use with heroku
-//console.log(d_key);
 
+const d_key = process.env.diskey; //for use with heroku
+
+//once the bot has initialized, send a message in console
 client.once('ready', () => {
     console.log('Ready!');
 });
 
 client.on('message', message => {
+    //if the message sent by player doesn't start with the prefix, do nothing
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+    //get the args from the command
     const args = message.content.slice(prefix.length).split(/ +/);
     const commandName = args.shift().toLowerCase();
-
+    
+    //find what to do with this command in the commands folder
     const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));;
-    if (!command) return;
-    try {
+    if (!command) return;//if command not found, return
+    try {//try executing command
         command.execute(message, args);
         console.log('Sent ' + commandName + '-->' + 'User:' + message.member.user.username + '||ID: ' + message.member.user.id);
-    } catch (error) {
+    } catch (error) {//if doesnt work send error
         console.error(error);
         message.channel.send('there was an error trying to execute that command!');
     }
