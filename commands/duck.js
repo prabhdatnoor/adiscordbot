798ba172a1276eafa18930765d64c5a75b6fe32f
Prabhnoor <>
2024-12-17 13:56:34 -0500

    added comments to duck.js and namo.js

--- commands/duck.js
+++ commands/duck.js
@@ -4,12 +4,15 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'duck',
-    description: 'ducks!',
-    execute(message, args) {
+    description: 'ducks! Send a picture of a random duck duck!',
+	
+    execute(message, args) { //send a request to the random-d.uk api
         (async() => {
             const response = await fetch('https://random-d.uk/api/random');
             const json = await response.json();
             const embed = new Discord.MessageEmbed();
+
+//send message
             embed.setColor(hexcode()).setTitle('Duck').setDescription('Boom!').setImage(json.url).setFooter(json.message);
 
             message.channel.send(embed);
